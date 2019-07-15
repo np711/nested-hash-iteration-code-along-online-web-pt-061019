0025bbb88c663cf339c93959fad408cfc1c76f7c
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-15 14:24:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def (contacts)= {"Jon Snow" => {
   name: "Jon", 
   email: "jon_snow@thewall.we",
   
-  favorite_ice_cream_flavors: ["chocolate","vanilla"]
+  favorite_ice_cream_flavors:["chocolate","vanilla"]
 },
 "Freddy Mercury" => { name: "Freddy", email:"freddy@mercury.com",
 favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
