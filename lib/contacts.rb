9941ec8ec43d92277712c7295be9077236e7f0d8
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-15 14:44:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,10 +14,13 @@ require 'pry'
   #   }
   # }
   
-def contacts["Jon Snow"]
-  each do |person,contact_details_hash|
-  binding.pry
-end 
+def contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
+end
 
   {"Jon Snow" => {
   name: "Jon", 
